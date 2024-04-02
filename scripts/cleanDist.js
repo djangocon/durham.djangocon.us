78bc0453a69ea934d1c9b1cd16885557dfb508b8
mtrythall <mtrythall@users.noreply.github.com>
2024-04-02 01:50:43 +0000

    deploy: 8e4d55ecd169556a5ebc6c1f440c1d94e8c52ced

--- scripts/cleanDist.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-if (fs.existsSync('dist')) {
-  fs.rmSync('dist', { recursive: true, force: true });
-  console.log('Cleared dist folder');
-} else {
-  console.log('dist folder does not exist');
-}
